90ef5cf0a37cfe370d8126028c11a36d2515e1da
Zenia <2013001116@my.xu.edu.ph>
2023-04-20 15:11:45 +0000

    what

--- src/components/HistoryBox.vue
+++ src/components/HistoryBox.vue
@@ -1,20 +1,30 @@
 <template>
-    <h3>History</h3>
+    <div>
+        <h3>History</h3>
+    </div>
 </template>
 
 <script>
 export default{
-    name: 'Article'
+    name: 'HistoryBox'
 }
 </script>
 
 <style scoped>
+div{
+    height: 100%;
+    width: 100%;
+    margin: 60px 18.5% 0px 18.5px;
+}
+
 h3{
     font-family: var(--header);
     font-weight: 600;
     font-size: 40px;
-    line-height: 50px;
     text-align: center;
     letter-spacing: 0.095em;
+
+    color: var(--prime);
+    text-transform: uppercase;
 }
 </style>
