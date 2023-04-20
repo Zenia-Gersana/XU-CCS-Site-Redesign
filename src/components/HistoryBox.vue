57ec1fc2ee0379adb28530e72159ee3ac139b634
Zenia <2013001116@my.xu.edu.ph>
2023-04-20 14:58:59 +0000

    piss

---
+++ src/components/HistoryBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3>History</h3>
+</template>
+
+<script>
+export default{
+    name: 'Article'
+}
+</script>
+
+<style scoped>
+h3{
+    font-family: var(--header);
+    font-weight: 600;
+    font-size: 40px;
+    line-height: 50px;
+    text-align: center;
+    letter-spacing: 0.095em;
+}
+</style>
